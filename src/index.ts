37a6c06bf425ee55f4a54538a4eb8028882b5f62
yusshu <yushuverduras1309@gmail.com>
2020-08-11 16:20:10 -0500

    Export all things

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,16 @@
-export default {
 
-    Namespace: require('./command/namespace'),
-    ICommandBuilder: require('./command/decorator/command.builder'),
-    Decorators: require('./command/decorator/command.decorator'),
-    ICommandHolder: require('./command/command.holder'),
-    Command: require('./command/command'),
-    CommandRegistry: require('./command/command.registry'),
-    CommandDispatcher: require('./command/command.dispatcher'),
-    CommandParser: require('./command/parser/command.parser'),
-    CommandService: require('./command/command.service'),
-    IBinder: require('./command/argument.provider/bind/binder'),
-    IArgumentProvider: require('./command/argument.provider/argument.provider'),
-    ArgumentInjector: require('./command/argument.provider/argument.injector'),
-    ConvertersModule: require('./command/argument.provider/converters.module'),
-    CommandExecutor: require('./command/executor/command.executor'),
-    Key: require('./command/identity/key').default.Key,
-    TypeLiteral: require('./command/identiy/key').TypeLiteral
-
-};
+export * from "./command/namespace";
+export * from "./command/decorator/command.builder";
+export * from "./command/decorator/command.decorator";
+export * from "./command/command.holder";
+export * from "./command/command";
+export * from "./command/command.registry";
+export * from "./command/command.dispatcher";
+export * from "./command/parser/command.parser";
+export * from "./command/command.service";
+export * from "./command/argument.provider/bind/binder";
+export * from "./command/argument.provider/argument.provider";
+export * from "./command/argument.provider/argument.injector";
+export * from "./command/argument.provider/converters.module";
+export * from "./command/executor/command.executor";
+export * from "./command/identity/key";
